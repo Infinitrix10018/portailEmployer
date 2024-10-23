4e7470e7f406a57860e5bb887b2dc9c7f92fca83
TheWiseToad <143433011+zexr50@users.noreply.github.com>
2024-10-23 15:06:37 -0400

    Update PreventBackHistory.php

--- app/Http/Middleware/PreventBackHistory.php
+++ app/Http/Middleware/PreventBackHistory.php
@@ -4,15 +4,23 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PreventBackHistory
 {
     public function handle(Request $request, Closure $next)
     {
+        
+        if (!Auth::check()) {
+            return redirect()->route('Accueil'); 
+        }
+
         $response = $next($request);
 
         return $response->header('Cache-Control', 'no-cache, no-store, must-revalidate')
                         ->header('Pragma', 'no-cache')
-                        ->header('Expires', '0'); 
+                        ->header('Expires', '0');
     }
+
+
 }
