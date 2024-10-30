c7da8ad31839fcbf995e8ff87519039be6ad8ab0
JonathanGremmoGit <117290338+JonathanGremmoGit@users.noreply.github.com>
2024-10-30 17:28:12 -0400

    Foreach fonctionnel

--- resources/views/views/pageChangementRole.blade.php
+++ resources/views/views/pageChangementRole.blade.php
@@ -9,25 +9,24 @@
 
                 <div class="container-xxl sub-bx" id="containerBlanc">
                     <div class="container-xxl">
+                    @csrf
                         <div class="row">
-
-                        <!--foreach ($users as $user)!-->
+                        @foreach ($users as $user)
                             <div class="col-md-5">
-                                <label for="province">Utilisateur:</label>
-                                <input type="text" class="form-control" value="placeholder">
-                                
+                                <label for="user_{{ $user->id }}">Utilisateur:</label>
+                                <p id="user_{{ $user->id }}" class="form-control-plaintext">{{ $user->name }}</p>
                             </div>
 
                             <div class="col-md-5">
-                                <label for="role">Role:</label>
-                                <select id="role" name="role" class="form-select" required>
-                                    <option value="Administrateur">Administrateur</option>
-                                    <option value="Commis">Commis</option>
-                                    <option value="Responsable">Responsable</option>
-                                    <option value="Aucun">Aucun</option>
+                                <label for="role_{{ $user->id }}">Role:</label>
+                                <select id="role_{{ $user->id }}" name="roles[{{ $user->id }}]" class="form-select" required>
+                                    <option value="Aucun" {{ $user->role === 'Aucun' ? 'selected' : '' }}>Aucun</option>
+                                    <option value="Administrateur" {{ $user->role === 'Administrateur' ? 'selected' : '' }}>Administrateur</option>
+                                    <option value="Commis" {{ $user->role === 'Commis' ? 'selected' : '' }}>Commis</option>
+                                    <option value="Responsable" {{ $user->role === 'Responsable' ? 'selected' : '' }}>Responsable</option>
                                 </select>
                             </div>
-                        <!--endforeach!-->
+                        @endforeach
 
                             <div class="col-sm-2">
                                 <div class="row">
@@ -38,7 +37,7 @@
                             </div>
 
                             <div class="col-lg-8">
-                                <button type="button" class="button">Enregistrer les modifications</button>
+                                <button type="button" class="button" id="idBoutonEnregistrer">Enregistrer les modifications</button>
                             </div>
 
                             <div class="col-sm-4">
